08630fb7cfb29e0cef640e45c3f69020a3579e86
Kristoffer Panahon <krp312@me.com>
2018-04-12 01:08:47 -0400

    Formatting

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -3,6 +3,6 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model(params) {
     let bands = this.modelFor('bands');
-    return bands.findBy('slug', params.slug); // of the band objects, find the one which matches params.slug, which is pearl-jam
+    return bands.findBy('slug', params.slug);
   }
 });
